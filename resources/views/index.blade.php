edc732cbfbb07ab773e45c896065571707f1bac1
Ignas <ignasstarzinskis@gmail.com>
2020-12-10 13:07:32 +0200

    homepage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.front')
+
+@section('content')
+    <section class="homepage vh-100 d-flex justify-content-center align-items-center">
+        <div class="content-holder">
+            <h1>ToDo list</h1>
+            <h2>made by: Ignas Staržinskis</h2>
+            <h2>technologies used: PHP 7.3, Git, MySql, Laravel, Vue.js, Vuex, Vue router</h2>
+            <div class="login-link-holder text-center mt-5">
+                <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
+            </div>
+        </div>
+    </section>
+@endsection
