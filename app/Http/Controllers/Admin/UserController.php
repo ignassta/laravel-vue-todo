835263506b3e23601ca70620c9a923cf91880c1d
Ignas <ignasstarzinskis@gmail.com>
2020-12-10 15:32:07 +0200

    created user controller

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::where('role', 2)->orderBy('created_at', 'desc')->get();
+
+        return response()->json($users);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'unique:users,email',
+            'password' => 'required'
+        ]);
+
+        $user = new User([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+        ]);
+
+        $user->save();
+
+        return response()->json($user);
+    }
+
+    public function edit($id)
+    {
+        $user = User::find($id);
+
+        return response()->json($user);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'unique:users,email'.$id,
+        ]);
+
+        $user->name = $request->get('name');
+        $user->email = $request->get('email');
+        $user->password = Hash::make($request->get('email'));
+        $user->save();
+
+        return response()->json($user);
+    }
+
+    public function destroy($id)
+    {
+        User::destroy($id);
+
+        return response()->json("ok");
+    }
+}
