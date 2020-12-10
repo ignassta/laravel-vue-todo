bfac09dc87f9ea6e1541e4c97646ec0a1459728b
Ignas <ignasstarzinskis@gmail.com>
2020-12-10 15:22:46 +0200

    created todo model, migration, seeder, factory

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Todo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TodoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Todo::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'task' => $this->faker->text(30),
+            'user_id' => rand(2,51),
+            'status' => rand(0,2),
+        ];
+    }
+}
---
+++ database/seeders/TodosTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Todo;
+use Illuminate\Database\Seeder;
+
+class TodosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Todo::factory(300)->create();
+    }
+}
